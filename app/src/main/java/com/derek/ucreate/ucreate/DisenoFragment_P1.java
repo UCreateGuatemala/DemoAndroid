bc25c31f92702dc8dda080220508890485fac9d2
borisfernando <boorisfernandoo@gmail.com>
2015-06-09 12:25:16 -0600

    Merge Test V.01

--- app/src/main/java/com/derek/ucreate/ucreate/DisenoFragment_P1.java
+++ app/src/main/java/com/derek/ucreate/ucreate/DisenoFragment_P1.java
@@ -5,7 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
@@ -13,15 +15,18 @@ import android.widget.TextView;
  */
 public class DisenoFragment_P1 extends Fragment {
 
-    ImageView   logoIcon;
-    TextView    logoText;
+    RelativeLayout  CatalogBackground;
+    Button          btnBackground, btnLogo, btnTitle, btnText;
+    ImageView       logoIcon;
+    TextView        logoText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.catalog_1,container);
         logoIcon = (ImageView) v.findViewById(R.id.imageViewCatalog1);
         logoText = (TextView) v.findViewById(R.id.textViewCatalog1);
-
+        logoIcon.setOnClickListener(new ClickListenerLogo());
+        logoText.setOnClickListener(new ClickListenerLogo());
 
         return v;
     }
@@ -34,5 +39,12 @@ public class DisenoFragment_P1 extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    class ClickListenerLogo implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+
+        }
     }
 }
