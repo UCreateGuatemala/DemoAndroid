63def44c266da6c83f3f7470b30f70d2ef9274ee
borisfernando <boorisfernandoo@gmail.com>
2015-06-09 12:55:50 -0600

    Merge Test V.03

--- app/src/main/java/com/derek/ucreate/ucreate/DisenoFragment_P1.java
+++ app/src/main/java/com/derek/ucreate/ucreate/DisenoFragment_P1.java
@@ -28,6 +28,9 @@ public class DisenoFragment_P1 extends Fragment {
     private TextView        logoText;
     private final int       PICK_IMAGE_REQUEST=1;
     private String          backgroundColor="white";
+    private String          textColor="black";
+    private ImageView       ivPosition1, ivPosition2, ivPosition3;
+    private TextView        tvPosition1, tvPosition2, tvPosition3, tvItem1, tvItem2;
 
 
     @Override
@@ -42,6 +45,9 @@ public class DisenoFragment_P1 extends Fragment {
         btnBackground = (Button) v.findViewById(R.id.buttonCatalogBackground);
         btnTitle = (Button) v.findViewById(R.id.buttonCatalogNewTitle);
         btnText = (Button) v.findViewById(R.id.buttonCatalogTextColor);
+
+        tvItem1 = (TextView) v.findViewById(R.id.textViewItem1);
+        tvItem2 = (TextView) v.findViewById(R.id.textViewItem2);
 
         btnLogo.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -143,9 +149,74 @@ public class DisenoFragment_P1 extends Fragment {
                 popup.show();
             }
         });
-
-
-
+        btnText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(getActivity().getApplicationContext(), v);
+                popup.inflate(R.menu.menu_change_text_color);
+                Object menuHelper;
+                Class[] argTypes;
+                try {
+                    Field fMenuHelper = PopupMenu.class.getDeclaredField("mPopup");
+                    fMenuHelper.setAccessible(true);
+                    menuHelper = fMenuHelper.get(popup);
+                    argTypes = new Class[] { boolean.class };
+                    menuHelper.getClass().getDeclaredMethod("setForceShowIcon", argTypes).invoke(menuHelper, true);
+                } catch (Exception e) {
+                    popup.show();
+                    return;
+                }
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.orange:
+                                tvItem1.setTextColor(getResources().getColor(R.color.orange));
+                                tvItem2.setTextColor(getResources().getColor(R.color.orange));
+                                textColor = "orange";
+                                return true;
+                            case R.id.red:
+                                tvItem1.setTextColor(getResources().getColor(R.color.red));
+                                tvItem2.setTextColor(getResources().getColor(R.color.red));
+                                textColor = "red";
+                                return true;
+                            case R.id.yellow:
+                                tvItem1.setTextColor(getResources().getColor(R.color.yellow));
+                                tvItem2.setTextColor(getResources().getColor(R.color.yellow));
+                                textColor = "yellow";
+                                return true;
+                            case R.id.green:
+                                tvItem1.setTextColor(getResources().getColor(R.color.green));
+                                tvItem2.setTextColor(getResources().getColor(R.color.green));
+                                textColor = "green";
+                                return true;
+                            case R.id.blue:
+                                tvItem1.setTextColor(getResources().getColor(R.color.blue));
+                                tvItem2.setTextColor(getResources().getColor(R.color.blue));
+                                textColor = "blue";
+                                return true;
+                            case R.id.purple:
+                                tvItem1.setTextColor(getResources().getColor(R.color.purple));
+                                tvItem2.setTextColor(getResources().getColor(R.color.purple));
+                                textColor = "purple";
+                                return true;
+                            case R.id.white:
+                                tvItem1.setTextColor(getResources().getColor(R.color.white));
+                                tvItem2.setTextColor(getResources().getColor(R.color.white));
+                                textColor = "white";
+                                return true;
+                            case R.id.black:
+                                tvItem1.setTextColor(getResources().getColor(R.color.black));
+                                tvItem2.setTextColor(getResources().getColor(R.color.black));
+                                textColor = "white";
+                                return true;
+                            default:
+                                return false;
+                        }
+                    };
+                });
+                popup.show();
+            }
+        });
         return v;
     }
 
