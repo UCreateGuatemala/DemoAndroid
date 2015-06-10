1b4c4d4c9a6e0097eed4f0c13d373fedf50a6b3d
dkoa <derekwoa@gmail.com>
2015-06-10 17:09:06 -0600

    Step 4 first modification

--- app/src/main/java/com/derek/ucreate/ucreate/Catalogs/Catalog_4.java
+++ app/src/main/java/com/derek/ucreate/ucreate/Catalogs/Catalog_4.java
@@ -14,6 +14,20 @@ import com.derek.ucreate.ucreate.R;
  */
 public class Catalog_4 extends Fragment {
 
+    int position = 2;
+    //String image;
+    String titleText = "UCreate";
+    int titleColor = 1;
+    int backgroundColor = 1;
+    int textColor = 1;
+
+    int template = 1;
+
+    int categoriesQuantity = 2;
+    int productsQuantity = 16;
+    Boolean rating = false, btnBuy = false, description = false, reviews = false;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.catalog_2, container, false);
