9403bb315ecda245cdf61a37223dc42febb0eb31
borisfernando <boorisfernandoo@gmail.com>
2015-06-10 15:55:52 -0600

    More Changes

--- app/src/main/java/com/derek/ucreate/ucreate/Catalogs/Catalog_2.java
+++ app/src/main/java/com/derek/ucreate/ucreate/Catalogs/Catalog_2.java
@@ -1,22 +1,16 @@
 package com.derek.ucreate.ucreate.Catalogs;
 
-import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.derek.ucreate.ucreate.R;
-
-import java.lang.reflect.Field;
 
 /**
  * Created by Derek on 5/27/2015.
@@ -74,5 +68,11 @@ public class Catalog_2 extends Fragment {
             }
         });
         return view;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Toast.makeText(getActivity(), getActivity().getIntent().getStringExtra("backgroundColor"), Toast.LENGTH_LONG).show();
     }
 }
